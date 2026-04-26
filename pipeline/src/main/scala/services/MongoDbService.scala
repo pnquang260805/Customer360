6c8e1f171a9804de41ff56e16c6d6849d38eb085
pnquang260805 <phamquang26805@gmail.com>
2026-04-26 08:52:16 +0700

    test

--- pipeline/src/main/scala/services/MongoDbService.scala
+++ pipeline/src/main/scala/services/MongoDbService.scala
@@ -10,23 +10,32 @@ import org.apache.spark.sql.types.StructType
 class MongoDbService(spark: SparkSession) {
   private val configVars = new ConfigVariables;
 
-  def writeMongoDb(df: DataFrame, database: String, collection: String, connectionString: String, idColumn: String, checkpoint: String): Unit = {
+  def writeMongoDb(
+      df: DataFrame,
+      database: String,
+      collection: String,
+      connectionString: String,
+      idColumn: String,
+      checkpoint: String
+  ): Unit = {
     // MongoDB Spark Connector (phiên bản V2/Spark 3) không hỗ trợ Output Mode là Update trực tiếp từ phương thức writeStream.
-    df.writeStream.foreachBatch(
-        (batchDf: DataFrame, batchId: Long) => {
-          batchDf.write.format("mongodb")
-            .option("forceDeleteTempCheckpointLocation", "true")
-            .option("spark.mongodb.connection.uri", connectionString)
-            .option("spark.mongodb.database", database)
-            .option("spark.mongodb.collection", collection)
-            .option("idFieldList", idColumn)
-            .option("operationType", "update")
-            .option("partitioner", "com.mongodb.spark.sql.connector.read.partitioner.SinglePartitionPartitioner")
-            .mode(SaveMode.Append)
-            .save();
-        }
-
-      )
+    df.writeStream
+      .foreachBatch((batchDf: DataFrame, batchId: Long) => {
+        batchDf.write
+          .format("mongodb")
+          .option("forceDeleteTempCheckpointLocation", "true")
+          .option("spark.mongodb.connection.uri", connectionString)
+          .option("spark.mongodb.database", database)
+          .option("spark.mongodb.collection", collection)
+          .option("idFieldList", idColumn)
+          .option("operationType", "update")
+          .option(
+            "partitioner",
+            "com.mongodb.spark.sql.connector.read.partitioner.SinglePartitionPartitioner"
+          )
+          .mode(SaveMode.Append)
+          .save();
+      })
       .option("checkpointLocation", checkpoint)
       .option("hoodie.metadata.enable", "false")
       .outputMode("update")
@@ -34,8 +43,14 @@ class MongoDbService(spark: SparkSession) {
     print("=====Inserted to mongodb=====")
   }
 
-  def readMongo(schema: StructType, database: String, collection: String, connectionString: String): DataFrame = {
-    return spark.read.format("mongodb")
+  def readMongo(
+      schema: StructType,
+      database: String,
+      collection: String,
+      connectionString: String
+  ): DataFrame = {
+    return spark.read
+      .format("mongodb")
       .option("spark.mongodb.connection.uri", connectionString)
       .option("spark.mongodb.database", database)
       .option("spark.mongodb.collection", collection)
